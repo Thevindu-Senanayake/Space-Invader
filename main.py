d6bde21961a9f458128695648dc6ebc6dd2c5e8b
Thevindu-Senanayake <thevindusenanayake76@gmail.com>
2022-02-07 08:18:44 +0530

    Added Bullet

--- main.py
+++ main.py
@@ -29,13 +29,28 @@ enemyImg = pygame.image.load("ufo.png")
 enemyX = random.randint(0, 735)
 enemyY = random.randint(50, 150)
 enemyX_change = 4
-enemyY_change = 40
+enemyY_change = 10
+
+# Bullet
+bulletImg = pygame.image.load("bullet.png")
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 40
+# Ready - You can't see the bullet on the screen
+# Fire - The bullet is currently moving
+bullet_state = "ready"
 
 def player(x, y):
 	screen.blit(playerImg, (x, y))
 
 def enemy(x, y):
 	screen.blit(enemyImg, (x, y))
+
+def fire_bullet(x, y):
+	global bullet_state
+	bullet_state = "fire"
+	screen.blit(bulletImg, (x + 16, y + 10))
 
 movement_speed = 5
 
@@ -58,6 +73,9 @@ while done == False:
 				playerX_change = -movement_speed
 			if event.key == pygame.K_RIGHT:
 				playerX_change = movement_speed
+			if event.key == pygame.K_SPACE:
+				fire_bullet(playerX, bulletY)
+
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
 				playerX_change = 0
@@ -84,6 +102,11 @@ while done == False:
 	elif enemyX >= 736:
 		enemyX_change = -4
 		enemyY += enemyY_change
+
+	# Move the bullet
+	if bullet_state is "fire":
+		fire_bullet(playerX, bulletY)
+		bulletY -= bulletY_change
 
 	player(playerX, playerY)
 	enemy(enemyX, enemyY)
